ffc5e274567eca6ce48243456139d2ab02e69415
Jazmin Bascuñan <jazbascunan01@gmail.com>
2025-04-24 14:17:16 -0300

    Se agrega JPAUtil.java y persistence.xml para configuración de JPA

---
+++ src/main/java/org/example/factory/JPAUtil.java
@@ -0,0 +1,17 @@
+package main.java.org.example.factory;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAUtil {
+    private static final EntityManagerFactory emf;
+
+    static {
+        emf = Persistence.createEntityManagerFactory("integrador2");
+    }
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+}
